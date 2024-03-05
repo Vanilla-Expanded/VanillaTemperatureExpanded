08cb47247e63bdd68cecbfb4497ddbebdf9a5d17
Linniel Driver-Williams <LinnielDW@googlemail.com>
2024-03-05 22:18:55 +0100

    added c# skeleton

---
+++ 1.4/Source/VanillaTemperatureExpanded/VanillaTemperatureExpanded.cs
@@ -0,0 +1,11 @@
+﻿using Verse;
+
+namespace VanillaTemperatureExpanded
+{
+    public class VanillaTemperatureExpanded : Mod
+    {
+        public VanillaTemperatureExpanded(ModContentPack content) : base(content)
+        {
+        }
+    }
+}
