22121a2ed349ab46b7dd36a43c4725d728f57ddb
Taranchuk <nfhfyxer@gmail.com>
2024-03-18 19:37:13 +0200

    added proxy heat

---
+++ 1.4/Source/ProxyHeat/Startup.cs
@@ -0,0 +1,68 @@
+﻿using HarmonyLib;
+using RimWorld;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Verse;
+
+namespace ProxyHeat
+{
+    [StaticConstructorOnStartup]
+	public static class Startup
+	{
+		static Startup()
+		{
+			foreach (var thingDef in DefDatabase<ThingDef>.AllDefs)
+			{
+				var props = thingDef.GetCompProperties<CompProperties_TemperatureSource>();
+				if (props is null)
+				{
+                    var compHeatPusher = thingDef.GetCompProperties<CompProperties_HeatPusher>();
+                    if (compHeatPusher != null)
+                    {
+                        props = new CompProperties_TemperatureSource();
+                        if (compHeatPusher.heatPerSecond > 0)
+                        {
+                            if (compHeatPusher.heatPushMaxTemperature != default && compHeatPusher.heatPushMaxTemperature != 99999f)
+                            {
+                                props.tempOutcome = compHeatPusher.heatPushMaxTemperature;
+                                props.maxTemperature = compHeatPusher.heatPushMaxTemperature;
+                            }
+                            else
+                            {
+                                props.tempOutcome = compHeatPusher.heatPerSecond * 5f;
+                            }
+                        }
+                        else
+                        {
+                            if (compHeatPusher.heatPushMinTemperature != default && compHeatPusher.heatPushMinTemperature != -99999f)
+                            {
+                                props.tempOutcome = compHeatPusher.heatPushMinTemperature;
+                                props.minTemperature = compHeatPusher.heatPushMinTemperature;
+                            }
+                            else
+                            {
+                                props.tempOutcome = compHeatPusher.heatPerSecond * 5f;
+                            }
+                        }
+                        props.dependsOnFuel = thingDef.GetCompProperties<CompProperties_Refuelable>() != null;
+                        props.dependsOnPower = thingDef.GetCompProperties<CompProperties_Power>()?.basePowerConsumption > 0;
+                        props.radius = ((thingDef.Size.x + thingDef.Size.z) / 2f) + 0.5f;
+                        props.smeltSnowRadius = props.radius;
+                        thingDef.comps.Add(props);
+                    }
+                    else
+                    {
+                        var compTarget = thingDef.GetCompProperties<CompProperties_TempControl>();
+                        if (compTarget != null) 
+                        {
+                            Log.Message("[VTE] Found no data for " + thingDef + " to autogenerate proxy heat");
+                        }
+                    }
+                }
+            }
+		}
+    }
+}
---
+++ 1.4/Source/VanillaTemperatureExpanded/Buildings/Building_TwoCellCooler.cs
@@ -0,0 +1,59 @@
+﻿using RimWorld;
+using UnityEngine;
+using Verse;
+
+namespace VanillaTemperatureExpanded.Buildings;
+
+public class Building_TwoCellCooler : Building_TempControl
+{
+    public override void TickRare()
+    {
+        if (!compPowerTrader.PowerOn)
+        {
+            return;
+        }
+        bool flag = PushCoolAir();
+        CompProperties_Power props = compPowerTrader.Props;
+        if (flag)
+        {
+            compPowerTrader.PowerOutput = 0f - props.PowerConsumption;
+        }
+        else
+        {
+            compPowerTrader.PowerOutput = (0f - props.PowerConsumption) * compTempControl.Props.lowPowerConsumptionFactor;
+        }
+        compTempControl.operatingAtHighPower = flag;
+    }
+
+    private bool PushCoolAir()
+    {
+        IntVec3 intVec = base.Position + IntVec3.South.RotatedBy(base.Rotation);
+        IntVec3 intVec2 = base.Position + IntVec3.North.RotatedBy(base.Rotation);
+        bool flag = false;
+        if (!intVec2.Impassable(base.Map) && !intVec.Impassable(base.Map))
+        {
+            float temperature = intVec2.GetTemperature(base.Map);
+            float temperature2 = intVec.GetTemperature(base.Map);
+            float num = temperature - temperature2;
+            if (temperature - 40f > num)
+            {
+                num = temperature - 40f;
+            }
+            float num2 = 1f - num * (1f / 130f);
+            if (num2 < 0f)
+            {
+                num2 = 0f;
+            }
+            float num3 = compTempControl.Props.energyPerSecond * num2 * 4.1666665f;
+            float num4 = GenTemperature.ControlTemperatureTempChange(intVec, base.Map, num3, compTempControl.targetTemperature);
+            flag = !Mathf.Approximately(num4, 0f);
+            if (flag)
+            {
+                intVec.GetRoom(base.Map).Temperature += num4;
+                GenTemperature.PushHeat(intVec2, base.Map, (0f - num3) * 1.25f);
+            }
+        }
+
+        return flag;
+    }
+}
