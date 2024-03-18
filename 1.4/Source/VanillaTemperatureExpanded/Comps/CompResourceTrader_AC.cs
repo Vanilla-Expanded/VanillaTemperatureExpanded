008d17f3adc0f26ba1b3106f9ebd4cca68040890
Linniel Driver-Williams <LinnielDW@googlemail.com>
2024-03-18 13:37:02 +0100

    added CompResourceTrader_Compressor to compressors

--- 1.4/Source/VanillaTemperatureExpanded/Comps/CompResourceTrader_AC.cs
+++ 1.4/Source/VanillaTemperatureExpanded/Comps/CompResourceTrader_AC.cs
@@ -1,8 +1,26 @@
-﻿using PipeSystem;
+﻿using System.Collections.Generic;
+using PipeSystem;
+using Verse;
 
 namespace VanillaTemperatureExpanded;
 
 public class CompResourceTrader_AC : CompResourceTrader
 {
     //TODO: implement
+}
+public class CompResourceTrader_Compressor : CompResourceTrader
+{
+    public override bool CanBeOn()
+    {
+        return base.CanBeOn() && !RoofUtility.IsAnyCellUnderRoof(parent);
+    }
+
+    public override void CompTickRare()
+    {
+        base.CompTickRare();
+        if (RoofUtility.IsAnyCellUnderRoof(parent))
+        {
+            this.ResourceOn = false;
+        }
+    }
 }
